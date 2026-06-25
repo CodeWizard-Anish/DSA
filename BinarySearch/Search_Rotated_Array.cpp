3263238d4ee4b53798081d021170412a2ba6e452
Anish <anishraj4343@gmail.com>
2026-06-25 13:15:46 +0530

    search in rotated sorted array

--- BinarySearch/Search_Rotated_Array.cpp
+++ BinarySearch/Search_Rotated_Array.cpp
@@ -33,7 +33,35 @@ int main(){
 
     // TODO:
     // Rotated Search Logic
+    int start = 0;
+    int end = nums.size() - 1;
 
+    while(start<=end){
+        int mid = start +(end  - start )/2;
+        if (nums[mid] == target){
+            cout<< mid << endl;
+            return 0;
+        } 
+
+            // Check if left part is sorted
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1; // move left
+                } else {
+                    start = mid + 1; // move right
+                }
+            } 
+            // Right part is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1; // move right
+                } else {
+                    end = mid - 1; // move left
+                }
+            }
+        }
+   
+        cout << -1 << endl; 
 
 
     return 0;
