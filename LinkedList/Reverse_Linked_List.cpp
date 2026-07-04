1024ed6599b861c8ae1f82b48478f38a00181b82
Anish <anishraj4343@gmail.com>
2026-07-04 21:58:51 +0530

    reverse a linked list done

--- LinkedList/Reverse_Linked_List.cpp
+++ LinkedList/Reverse_Linked_List.cpp
@@ -36,17 +36,57 @@ public:
         next = nullptr;
     }
 };
+Node* createLinkedList(int arr[], int n) {
+    if (n == 0) return nullptr;
 
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+
+    for (int i = 1; i < n; ++i) {
+        current->next = new Node(arr[i]);
+        current = current->next;
+    }
+
+    return head;
+}
+Node* reverseLinkedList(Node* head) {
+    Node* prev = nullptr;
+    Node* current = head;
+    Node* next = nullptr;
+
+    while (current != nullptr) {
+        next = current->next; // Store next node
+        current->next = prev; // Reverse the link
+        prev = current;       // Move prev to current
+        current = next;       // Move to next node
+    }
+
+    return prev; // New head of the reversed list
+}
+Node* printLinkedList(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+    return head;
+}
 int main(){
 
     // TODO:
     // Create linked list
+    int arr[] = {1, 2, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Node* head = createLinkedList(arr, n);
 
     // TODO:
     // Reverse list
+    head = reverseLinkedList(head);
 
     // TODO:
     // Print
+    printLinkedList(head);
 
     return 0;
 }
