33c433c7f77ce9fa408d37a83625930da9d7e8c7
Anish <anishraj4343@gmail.com>
2026-07-05 17:26:32 +0530

    Odd Even Linked List Done

--- LinkedList/Odd_Even_Linked_List.cpp
+++ LinkedList/Odd_Even_Linked_List.cpp
@@ -39,6 +39,30 @@ int main(){
 
     // TODO:
     // Rearrange list
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    Node* odd = head;
+    Node* even = head->next;
+    Node* evenHead = even; // Store the head of even nodes
+
+    while (even != nullptr && even->next != nullptr) {
+        odd->next = even->next; // Link odd node to the next odd node
+        odd = odd->next;        // Move odd pointer to the next odd node
+        even->next = odd->next; // Link even node to the next even node
+        even = even->next;      // Move even pointer to the next even node
+    }
+    odd->next = evenHead; // Link the last odd node to the head of even nodes   
+    cout << "Rearranged list (odd followed by even): ";
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
 
     return 0;
 }
