b710a73173a1bb65bc4915209cb6c8bf4ebafce2
Anish <anishraj4343@gmail.com>
2026-07-06 13:01:24 +0530

    sort list done

--- LinkedList/Sort_Linked_List.cpp
+++ LinkedList/Sort_Linked_List.cpp
@@ -34,11 +34,64 @@ public:
         next=nullptr;
     }
 };
+Node* merge(Node* l1, Node* l2) {
+    Node* dummy = new Node(0);
+    Node* tail = dummy;
 
+    while(l1 != nullptr && l2 != nullptr) {
+        if(l1->data < l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+    if(l1 != nullptr) tail->next = l1;
+    if(l2 != nullptr) tail->next = l2;
+
+    return dummy->next;
+}
+Node* getMiddle(Node* head) {
+    if(head == nullptr) return head;
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while(fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+Node* sortList(Node* head) {
+    if(head == nullptr || head->next == nullptr) return head;
+
+    Node* middle = getMiddle(head);
+    Node* nextToMiddle = middle->next;
+    middle->next = nullptr;
+
+    Node* left = sortList(head);
+    Node* right = sortList(nextToMiddle);
+
+    return merge(left, right);
+}
 int main(){
 
     // TODO:
     // Merge sort
+    Node* head = new Node(4);
+    head->next = new Node(2);
+    head->next->next = new Node(1);
+    head->next->next->next = new Node(3);
+
+    head = sortList(head);
+    cout << "Sorted List: ";
+    Node* current = head;
+    while(current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
 
     return 0;
 }
