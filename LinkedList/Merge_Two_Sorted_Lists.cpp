0a4e3bbc716ad625a89285fdd44b93607b372cfa
Anish <anishraj4343@gmail.com>
2026-07-06 12:45:10 +0530

    Merge sorted linkedlist done

--- LinkedList/Merge_Two_Sorted_Lists.cpp
+++ LinkedList/Merge_Two_Sorted_Lists.cpp
@@ -39,6 +39,40 @@ int main(){
 
     // TODO:
     // Merge two lists
+    Node* l1 = new Node(1);
+    l1->next = new Node(3); 
+    l1->next->next = new Node(5);
 
+    Node* l2 = new Node(2);
+    l2->next = new Node(4);
+    l2->next->next = new Node(6);
+
+    Node* dummy = new Node(0);
+    Node* tail = dummy;
+    Node* p1 = l1;
+    Node* p2 = l2;
+
+    while(p1 != nullptr && p2 != nullptr) {
+        if(p1->data < p2->data) {
+            tail->next = p1;
+            p1 = p1->next;
+        } else {
+            tail->next = p2;
+            p2 = p2->next;
+        }
+        tail = tail->next;
+    }
+    // Append any remaining nodes from either list
+    if(p1 != nullptr) {
+        tail->next = p1;
+    } else {
+        tail->next = p2;
+    }
+    cout << "Merged List: ";
+    Node* current = dummy->next;
+    while(current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
     return 0;
 }
