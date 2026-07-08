2028b7260f59e1c011c6d18710564676ff57e3b1
Anish <anishraj4343@gmail.com>
2026-07-08 10:10:45 +0530

    Reverse Array using recursion done

--- Recursion/Reverse_Array.cpp
+++ Recursion/Reverse_Array.cpp
@@ -29,6 +29,12 @@ using namespace std;
 void reverseArray(vector<int>& nums,int left,int right){
 
     // TODO:
+    if(left >= right) {
+        return; // Base case: if left index is greater than or equal to right index, return
+    }
+    swap(nums[left], nums[right]); // Swap the elements at left and right indices
+
+    reverseArray(nums, left + 1, right - 1); // Recursive call: move towards the center of the array
 
 }
 
@@ -37,6 +43,11 @@ int main(){
     vector<int> nums={1,2,3,4,5};
 
     reverseArray(nums,0,nums.size()-1);
+    cout<<"Reversed Array: ";
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" "; // Print the reversed array
+    }
+    cout<<endl;
 
     return 0;
 }
