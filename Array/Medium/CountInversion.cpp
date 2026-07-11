ff17bab5e2de4c557ce2a8d2332d2191a1073592
Anish <anishraj4343@gmail.com>
2026-07-11 22:08:38 +0530

    Count Inversion done

--- Array/Medium/CountInversion.cpp
+++ Array/Medium/CountInversion.cpp
@@ -27,18 +27,69 @@ Space: O(n)
 #include <vector>
 using namespace std;
 
-long long mergeSort(vector<int>& arr,int low,int high){
+long long merge(vector<int>& arr, int low, int mid, int high) {
 
-    // TODO
+    vector<int> temp;
 
-    return 0;
+    int left = low;
+    int right = mid + 1;
+
+    long long count = 0;
+
+    while (left <= mid && right <= high) {
+
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            count += (mid - left + 1);
+
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return count;
 }
 
-int main(){
+long long mergeSort(vector<int>& arr, int low, int high) {
+
+    if (low >= high)
+        return 0;
+
+    int mid = low + (high - low) / 2;
+
+    long long count = 0;
+
+    count += mergeSort(arr, low, mid);
+
+    count += mergeSort(arr, mid + 1, high);
+
+    count += merge(arr, low, mid, high);
+
+    return count;
+}
+
+int main() {
 
     vector<int> arr = {5,3,2,4,1};
 
-    cout << mergeSort(arr,0,arr.size()-1);
+    cout << mergeSort(arr, 0, arr.size() - 1);
 
     return 0;
 }
