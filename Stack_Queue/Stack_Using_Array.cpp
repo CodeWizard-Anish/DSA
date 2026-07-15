fe1b1a4372533a6c1ef934809229eac40099aec0
Anish <anishraj4343@gmail.com>
2026-07-15 18:34:54 +0530

    Implementation of stack using array

--- Stack_Queue/Stack_Using_Array.cpp
+++ Stack_Queue/Stack_Using_Array.cpp
@@ -31,20 +31,53 @@ class Stack{
 
     // TODO:
     // data members
+    int *arr;
+    int topIndex;
+    int size;
 
 public:
 
     // TODO:
     // push()
+    Stack(int s){
+        size = s;
+        arr = new int[size];
+        topIndex = -1;
+    }
+    void push(int x){
+        if(topIndex == size - 1){
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        arr[++topIndex] = x;
+    }
+
 
     // TODO:
     // pop()
+    void pop(){
+        if(topIndex == -1){
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        topIndex--;
+    }
 
     // TODO:
+    int top(){
+        if(topIndex == -1){
+            cout << "Stack is Empty" << endl;
+            return -1; // or throw an exception
+        }
+        return arr[topIndex];
+    }
     // top()
 
     // TODO:
     // empty()
+    bool empty(){
+        return topIndex == -1;
+    }
 
 };
 
@@ -52,6 +85,12 @@ int main(){
 
     // TODO:
     // Test Stack
+    Stack s(5);
+    s.push(10);
+    s.push(20);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
 
     return 0;
 }
